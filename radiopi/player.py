6bafd04ef71a50e31d55ae173ec6ca1853ae9084
Jason Heard <jasonpheard@gmail.com>
2015-09-07 09:30:36 -0600

    Add code to play special stream with description

--- radiopi/player.py
+++ radiopi/player.py
@@ -15,9 +15,12 @@ import streams
 
 
 class RadioPlayer:
-    def __init__(self):
+    def __init__(self, specialUrl=None, specialDescription=None):
         self.currentStationName = None
         self.playCount = 0
+        self.specialUrl = specialUrl
+        self.specialDescription = specialDescription
+        self.specialNext = True
         self.lock = threading.Lock()
         self.mpdClient = MPDClient()
         self.mpdClient.connect("localhost", 6600)
@@ -27,8 +30,14 @@ class RadioPlayer:
 
     def nextStation(self):
         if self.playCount < 1:
-            self.playStation(streams.getRandomStation())
-            self.playCount += 1
+            if self.specialUrl is not None and self.specialNext:
+                self.playUrl(self.specialUrl, self.specialDescription)
+                self.playCount += 1
+                self.specialNext = False
+            else:
+                self.playStation(streams.getRandomStation())
+                self.playCount += 1
+                self.specialNext = True
         else:
             self.stopPlaying()
             self.playCount = 0
@@ -54,6 +63,26 @@ class RadioPlayer:
             self.lock.release()
 
 
+    def playUrl(self, url, description):
+        self.lock.acquire()
+        try:
+            if self.currentStationName is not None:
+                self.__stop__()
+
+            self.mpdClient.add(url)
+
+            time.sleep(0.2)
+            espeak.synth(description)
+            while espeak.is_playing():
+                time.sleep(0.2)
+
+            self.mpdClient.play()
+
+            self.currentStationName = description
+        finally:
+            self.lock.release()
+
+
     def stopPlaying(self):
         self.lock.acquire()
         try:
@@ -68,4 +97,5 @@ class RadioPlayer:
         self.mpdClient.stop()
         self.mpdClient.clear()
         self.currentStationName = None
+        self.playCount = 0
 
