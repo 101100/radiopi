a0f881fb640b6dadf3304e8a590d9483f09089cf
Jason Heard <jasonpheard@gmail.com>
2017-12-26 20:05:24 -0700

    Add scripts to run radiopi

---
+++ runRadioPi.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# works for execution and for sourcing
+SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd )
+
+export PYTHONPATH="${SCRIPT_PATH}"
+export AWS_DEFAULT_REGION="us-west-2"
+export AWS_ACCESS_KEY_ID="your key"
+export AWS_SECRET_ACCESS_KEY="your secret"
+
+python -m radiopi
---
+++ tmuxRadioPi.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# works for execution and for sourcing
+SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd )
+
+tmux new-session -d -s radiopi
+tmux send-keys ". \"${SCRIPT_PATH}/runRadioPi.sh\"" C-m
+
